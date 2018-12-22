486a6ccbd60286fabf8af4c45b0014b354d1f4bb
Vitaliy <puga.vitaliy@gmail.com>
2018-12-22 22:49:22 +0200

    computed styles of element img

--- src/App.js
+++ src/App.js
@@ -3,38 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import TextContent from './components/TextContent';
 import FixedBlock from './components/FixedBlock';
+import Modal from './components/Modal/Modal';
+
+
 
 class App extends Component {
 
   state = {
-    popup: false
+    popup: false,
+    modal: false,
+    toggle: 0,
   }
 
   constructor(props) {
     super(props);
-
-    this.showPopup = this.showPopup.bind(this);
-    this.closePopup = this.closePopup.bind(this);
   }
 
-  showPopup(e) {
-    e.preventDefault();
+  componentDidMount() {
+    let cli = this.cli;
+    this.toggle.onload = function() {
+      let styleH = this.height + 'px';
+      let styleW = this.width + 'px';
 
-    this.setState({
-      popup: true
-    }, () => {
-      document.addEventListener('click', this.closePopup);
-    });
-  }
-
-  closePopup(e) {
-    if (!this.togglePopup.contains(e.target)) {
-      this.setState({
-        popup: false
-      }, () => {
-        document.removeEventListener('click', this.closePopup);
-      });
+      cli.style.height = styleH;
+      cli.style.width = styleW;
+       
     }
+    
+  }
+
+  modalHandler() {
+    this.setState({modal: true});
   }
 
   render() {
@@ -54,6 +53,27 @@ class App extends Component {
             Learn React
 
           </a>
+
+          <img  
+            src={require('./Mockup-Generated-by-Dunnnk.jpg')} 
+            ref={elem => {
+              this.toggle = elem
+            }}
+          />
+          <img 
+            src={require('./screenSocialDev.png')} 
+            ref={elem => {
+              this.cli = elem
+            }}
+          />
+          <Modal show={this.state.modal}>
+            Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. Modal window, it have to help me with my task. 
+          </Modal>
+          <button
+            className="OrderButton"
+            onClick={this.modalHandler.bind(this)}
+            // onClick={}
+          >ORDER NOW</button>
           <TextContent />
           <FixedBlock />
         </header>
